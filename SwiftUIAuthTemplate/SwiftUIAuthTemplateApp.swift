a1c63a5cd6e0b36908876a8f283aa62d96e54273
Gerald <gbigens@gmai.com>
2022-09-04 11:06:47 +0300

    Initial Commit

---
+++ SwiftUIAuthTemplate/SwiftUIAuthTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAuthTemplateApp.swift
+//  SwiftUIAuthTemplate
+//
+//  Created by Gbrigens on 04/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAuthTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
